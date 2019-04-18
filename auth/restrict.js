56f3fadfc628e30fe135d8194143023f343f0edf
ajb85 <ajbrush85@gmail.com>
2019-04-18 15:24:42 -0400

    Added restrict console logs to see where Irving is having issues

--- auth/restrict.js
+++ auth/restrict.js
@@ -3,17 +3,22 @@ const secret = require("./secrets").jwtSecret;
 
 module.exports = (req, res, next) => {
   const token = req.body.token ? req.body.token : req.headers.Authorization;
+  console.log("RESTRICT TOKEN: ", token);
   if (token) {
+    console.log("Token Exists");
     jwt.verify(token, secret, (err, decodedToken) => {
       if (err) {
+        console.log("Token Failed");
         res.status(401).json({ message: "Invalid Credentials" });
       } else {
+        console.log("Token passed");
         req.decoded = decodedToken;
         delete req.body.token;
         next();
       }
     });
   } else {
+    console.log("Token Does Not Exists");
     res.status(401).json({ message: "No token provided!" });
   }
 };
